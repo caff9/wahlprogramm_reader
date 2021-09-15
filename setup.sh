3d960918f45c9033e4e8acd25ea315d8fb28727e
erikk <erik.klemusch@googlemail.com>
2021-09-15 22:42:11 +0200

    another try for the scheme on heroku

--- setup.sh
+++ setup.sh
@@ -4,5 +4,10 @@ echo "\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+[theme]\n\
+backgroundColor='#FFFFFF'\n\
+primaryColor='#22bd0a'\n\
+secondaryBackgroundColor='#d2d2d2'\n\
+textColor='#000000'\n\
 \n\
 " > ~/.streamlit/config.toml
